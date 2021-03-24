6c447c36c2f39ba45a7c3a99b516a2eac9bc87b5
Rasmus <rasmuswium@hotmail.com>
2021-03-24 09:41:19 +0100

    added sln

---
+++ ChargingStation/ChargingStation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChargingStation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
