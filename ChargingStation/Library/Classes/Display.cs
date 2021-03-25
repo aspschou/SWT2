3a6f9c507491fbec14c8a02ef8e8e23f47f7a900
Anders Schou <aspschou@live.dk>
2021-03-25 22:18:29 +0100

    Implemented Display

--- ChargingStation/Library/Classes/Display.cs
+++ ChargingStation/Library/Classes/Display.cs
@@ -7,7 +7,42 @@ using Library.Interfaces;
 
 namespace Library
 {
-    class Display
+     class Display : IDisplay
     {
+
+        public void DisplayMsg(MessageType messageType)
+        {
+
+            switch(messageType)
+            {
+                case MessageType.ConnectPhone:
+                    Console.WriteLine("Connect your phone");
+                    break;
+                case MessageType.ScanRFID:
+                    Console.WriteLine("Scan your RFID tag");
+                    break;
+                case MessageType.ConnectionError:
+                    Console.WriteLine("Connection error");
+                    break;
+                case MessageType.StationOccupied:
+                    Console.WriteLine("Station already in use");
+                    break;
+                case MessageType.RFIDError:
+                    Console.WriteLine("Tag can't be read");
+                    break;
+                case MessageType.TakePhone:
+                    Console.WriteLine("You may take your phone");
+                    break;
+                case MessageType.IsCharging:
+                    Console.WriteLine("The phone is currently charging");
+                    break;
+                case MessageType.FullyCharged:
+                    Console.WriteLine("The phone is fully charged. You can now remove it from the station");
+                    break;
+                case MessageType.CurrentWarning:
+                    Console.WriteLine("SOMETHING WENT WRONG, REMOVE PHONE");
+                    break;
+            }
+        }
     }
 }
