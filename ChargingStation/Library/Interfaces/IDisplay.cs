5b24034145a2de886c41d20c5c0b22fd14c6b04f
Anders Schou <aspschou@live.dk>
2021-03-25 19:44:06 +0100

    Added IDisplay.

--- ChargingStation/Library/Interfaces/IDisplay.cs
+++ ChargingStation/Library/Interfaces/IDisplay.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Library
+namespace Library.Interfaces
 {
-    class IDisplay
+    public interface IDisplay
     {
+        void Display(Enum MessageType);
     }
 }
