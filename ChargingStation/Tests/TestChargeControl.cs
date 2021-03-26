0227c2ac813b2e8c444af90eea916449208313d2
Rasmus <rasmuswium@hotmail.com>
2021-03-26 15:29:35 +0100

    Added more test files

---
+++ ChargingStation/Tests/TestChargeControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Library;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestChargeControll
+    {
+        //private ChargeControl _uut;
+        [SetUp]
+        public void Setup()
+        {
+            //_uut = new ChargeControl();
+        }
+    }
+}
---
+++ ChargingStation/Tests/TestStationControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Library;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestStationControl
+    {
+        //private StationControl _uut;
+        [SetUp]
+        public void Setup()
+        {
+            //_uut = new StationControl();
+        }
+    }
+}
